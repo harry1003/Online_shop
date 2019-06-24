2aeb12e94005fcd762e51307603f28d8b0670c56
harry1003 <b05901059@ntu.edu.tw>
2019-06-24 23:08:30 +0800

    fix purchase dot

--- client/src/containers/Header/Header.js
+++ client/src/containers/Header/Header.js
@@ -6,25 +6,30 @@ import logo from "../../resource/H.png";
 class Header extends Component {
     render() {
         let Item = "Item-buy";
-        if(Object.keys(this.props.shop_list).length > 0){
-            Item = Item + " Visible";
-        }
+        Object.keys(this.props.shop_list).some(
+            (item) => {
+                if(this.props.shop_list[item] > 0){
+                    Item = Item + " Visible";
+                    return true;
+                }
+            }
+        )
 
         return(
-        <div className="Header">
-            <div className="Blog_logo_name">
-                <img className="Logo" src={logo}/>
-                <div className="Line"></div>
-                <h1 className="Name">books shop</h1>
+            <div className="Header">
+                <div className="Blog_logo_name">
+                    <img className="Logo" src={logo}/>
+                    <div className="Line"></div>
+                    <h1 className="Name">books shop</h1>
+                </div>
+                <div className="Sign_in">
+                    <div className="Button">Sign in</div>
+                    <div className="Button-wrapper">
+                        <NavLink className="Button-black" to={"/purchase"}>Purchase</NavLink>
+                        <div className={Item}></div>
+                    </div>  
+                </div>
             </div>
-            <div className="Sign_in">
-                <div className="Button">Sign in</div>
-                <div className="Button-wrapper">
-                    <NavLink className="Button-black" to={"/purchase"}>Purchase</NavLink>
-                    <div className={Item}></div>
-                </div>  
-            </div>
-        </div>
         );
     }
 }
